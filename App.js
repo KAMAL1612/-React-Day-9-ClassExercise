a57c9ad13e656366025fb89eec29bc754b1c4e2b
KAMAL1612 <128037521+KAMAL1612@users.noreply.github.com>
2023-04-05 09:26:58 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar';
+function App() {
+return (
+    <div className="App">
+          <Navbar/>
+    </div>
+  );
+}
+
+export default App;
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
